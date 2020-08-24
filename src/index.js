62cf89522e7d72bebb101c66c016317b4e0e391e
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-25 00:02:03 +0530

    Update typeDefs

--- src/index.js
+++ src/index.js
@@ -1,37 +1,7 @@
 import { GraphQLServer } from "graphql-yoga";
 
 import { users, blogs } from "./datasets";
-
-// Types in GraphQL =>
-// Scalar types - String, Boolean, Int, Float, ID
-
-//type definitions (application schema)
-const typeDefs = `
-    type Query {
-        greeting(name: String, company: String): String!
-        add(a: Float!, b: Float!): Float!
-        addArray(nums: [Float!]!): Float!
-        grades: [Int!]!
-        userDetails: User!
-        users(search: String): [User!]!
-        blogDetails: Blog!
-        blogs(search: String): [Blog!]!
-    }
-
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-        age: Int
-    }
-
-    type Blog {
-        id: ID!
-        title: String!
-        body: String!
-        published: Boolean!
-    }
-`;
+import { typeDefs } from "./typeDefinitions";
 
 //resolvers
 const resolvers = {
---
+++ src/typeDefinitions.js
@@ -0,0 +1,30 @@
+// Types in GraphQL =>
+// Scalar types - String, Boolean, Int, Float, ID
+
+//type definitions (application schema)
+export const typeDefs = `
+    type Query {
+        greeting(name: String, company: String): String!
+        add(a: Float!, b: Float!): Float!
+        addArray(nums: [Float!]!): Float!
+        grades: [Int!]!
+        userDetails: User!
+        users(search: String): [User!]!
+        blogDetails: Blog!
+        blogs(search: String): [Blog!]!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Blog {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    }
+`;

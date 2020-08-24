d3cb9d67e347618007132fd68a01d94514a76fdc
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-24 23:48:12 +0530

    Update datasets

---
+++ src/datasets.js
@@ -0,0 +1,27 @@
+// user data
+export const users = [
+  { id: "id_1", name: "aBc", email: "abc@gmail.com", age: 3 },
+  { id: "id_2", name: "pQr", email: "pqr@gmail.com" },
+  { id: "id_3", name: "xYz", email: "xyz@gmail.com" },
+];
+
+export const blogs = [
+  {
+    id: "blog_1",
+    title: "Football legends",
+    body: "7 vs 13 it is!!!",
+    published: false,
+  },
+  {
+    id: "blog_2",
+    title: "Alpha treatment",
+    body: "A breakthrough in cancer treatment",
+    published: false,
+  },
+  {
+    id: "blog_3",
+    title: "Programming for everybody",
+    body: "Learn the right way",
+    published: false,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
 
+import { users, blogs } from "./datasets";
+
 // Types in GraphQL =>
 // Scalar types - String, Boolean, Int, Float, ID
-
-// user data
-const users = [
-  { id: "id_1", name: "aBc", email: "abc@gmail.com", age: 3 },
-  { id: "id_2", name: "pQr", email: "pqr@gmail.com" },
-  { id: "id_3", name: "xYz", email: "xyz@gmail.com" },
-];
-
-const blogs = [
-  {
-    id: "blog_1",
-    title: "Football legends",
-    body: "7 vs 13 it is!!!",
-    published: false,
-  },
-  {
-    id: "blog_2",
-    title: "Alpha treatment",
-    body: "A breakthrough in cancer treatment",
-    published: false,
-  },
-  {
-    id: "blog_3",
-    title: "Programming for everybody",
-    body: "Learn the right way",
-    published: false,
-  },
-];
 
 //type definitions (application schema)
 const typeDefs = `

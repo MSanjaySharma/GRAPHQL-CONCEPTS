090042005f88c88e107c346f4202f4217fc13907
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-25 00:03:57 +0530

    Update resolvers

--- src/index.js
+++ src/index.js
@@ -1,73 +1,8 @@
 import { GraphQLServer } from "graphql-yoga";
 
-import { users, blogs } from "./datasets";
 import { typeDefs } from "./typeDefinitions";
+import { resolvers } from "./resolvers";
 
-//resolvers
-const resolvers = {
-  Query: {
-    greeting(parent, args, ctx, info) {
-      if (args.name && args.company) {
-        return `Hello ${args.name} welcome to ${args.company}!!!`;
-      } else {
-        return `Hello !!!`;
-      }
-    },
-    add(parent, args, ctx, info) {
-      if (args.a && args.b) {
-        //`Sum of ${args.a} and ${args.b} is ${args.a + args.b}`;
-        return args.a + args.b;
-      }
-    },
-    addArray(parent, args, ctx, info) {
-      if (args.nums.length === 0) {
-        return 0;
-      } else {
-        return args.nums.reduce((accumulator, currentValue) => {
-          return accumulator + currentValue;
-        });
-      }
-    },
-    grades(parent, args, ctx, info) {
-      return [99, 88, 67];
-    },
-    users(parent, args, ctx, info) {
-      if (!args.search) {
-        return users;
-      } else {
-        return users.filter((user) => {
-          return user.name.toLowerCase().includes(args.search.toLowerCase());
-        });
-      }
-    },
-    userDetails(parent, args, ctx, info) {
-      return {
-        id: "userId_1",
-        name: "GXP",
-        email: "gxp@gmail.com",
-        age: 31,
-      };
-    },
-    blogs(parent, args, ctx, info) {
-      if (!args.search) {
-        return blogs;
-      } else {
-        const search = args.search;
-        return blogs.filter((blog) => {
-          return blog.title.includes(search) || blog.body.includes(search);
-        });
-      }
-    },
-    blogDetails(parent, args, ctx, info) {
-      return {
-        id: "blogId_1",
-        title: "Example Blog",
-        body: "This is example content",
-        published: false,
-      };
-    },
-  },
-};
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
